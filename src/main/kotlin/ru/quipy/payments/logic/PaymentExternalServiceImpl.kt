627cd8acae54afbb08e06307df3bc84c38268a65
Fisenko Nikita <nikitafisenko717@gmail.com>
2025-02-21 16:39:27 +0300

    case-1

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -6,11 +6,13 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
+import ru.quipy.common.utils.TokenBucketRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
 import java.time.Duration
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 // Advice: always treat time as a Duration
@@ -31,6 +33,7 @@ class PaymentExternalSystemAdapterImpl(
     private val requestAverageProcessingTime = properties.averageProcessingTime
     private val rateLimitPerSec = properties.rateLimitPerSec
     private val parallelRequests = properties.parallelRequests
+    private val tokenBucketRateLimiter = TokenBucketRateLimiter(rateLimitPerSec, rateLimitPerSec, requestAverageProcessingTime.toMillis(), TimeUnit.MILLISECONDS)
 
     private val client = OkHttpClient.Builder().build()
 
@@ -52,6 +55,7 @@ class PaymentExternalSystemAdapterImpl(
         }.build()
 
         try {
+            tokenBucketRateLimiter.tickBlocking()
             client.newCall(request).execute().use { response ->
                 val body = try {
                     mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
